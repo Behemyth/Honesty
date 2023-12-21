c074ac4d4fc8415da28e779f7e07439ecedc6c28
Asher Norland <asher.norland@gmail.com>
2023-12-21 16:11:40 -0500

    Test jaaaaaa

--- tests/unit/test_suite.cpp
+++ tests/unit/test_suite.cpp
@@ -1,20 +1,15 @@
-// import std;
-// import synodic.honesty.test;
-// import synodic.honesty.test.backend;
-// import generator;
-// using namespace synodic::honesty;
-//
-// namespace
-// {
-// 	auto suiteGenerator = []() -> TestGenerator
-// 	{
-// 		co_yield Test(
-// 			"top level test",
-// 			[]()
-// 			{
-// 			});
-// 	};
-//
-// 	// Registration
-// 	suite suite("suite suite", suiteGenerator);
-// }
+import std;
+import synodic.honesty.test;
+
+using namespace synodic::honesty;
+
+namespace
+{
+	auto suiteGenerator = []() -> TestGenerator
+	{
+		co_return;
+	};
+
+	// Verify that the suite is compiling
+	suite suite("suite suite", suiteGenerator);
+}
--- tests/unit/test_test.cpp
+++ tests/unit/test_test.cpp
@@ -1,50 +1,82 @@
-// These tests are not executed, so they exist to test the compilation of tests
+#include <cassert>
+#include <cstdio>
 
 import std;
 import synodic.honesty.test;
-import synodic.honesty.test.backend;
-import generator;
 
 using namespace synodic::honesty;
 using namespace synodic::honesty::literals;
 
-auto outerSetGenerator = []()
+template<class... Args>
+void expect(bool value, const std::source_location location, std::format_string<Args...> fmt, Args&&... args)
 {
-};
+	std::println(
+		stderr,
+		"Test Failed: File({}), Line({})",
+		location.file_name(),
+		location.line());
+	std::print(stderr, "    ");
+	std::println(stderr, fmt, std::forward<Args>(args)...);
+}
 
-auto suiteGenerator = []() -> TestGenerator
+namespace
 {
-	// Tests that creation via literal works
-	//auto innerSetGenerator = []() -> TestGenerator
-	//{
-	//	int count = 0;
-	//	co_yield Test(
-	//		"test",
-	//		[]
-	//		{
-	//		});
+	auto basicGenerator = []() -> TestGenerator
+	{
+		co_return;
+	};
 
-	//	// co_yield "yes"_test = [&count]()
-	//	//{
-	//	//	++count;
-	//	// };
+	using Function = std::move_only_function<TestGenerator()>;
 
-	//	// co_yield "array"_test = [&count](const auto& parameter)
-	//	//{
-	//	//	++count;
-	//	// } | std::tuple{3u, 4.0f};
+	struct Runner
+	{
+		explicit Runner(
+			Function function,
+			const int expected,
+			const std::source_location& location =
+				std::source_location::current())
+		{
+			int count = 0;
+			for (const auto& test: function())
+			{
+				++count;
+			}
 
-	//	// co_yield "array"_test = [&count]<typename T>(const T& parameter)
-	//	//{
-	//	//	++count;
-	//	// } | std::array{3, 4};
-	//};
+			expect(count == expected, location, "{} values were generated. {} was expected", count, expected);
+		}
+	};
 
-	//co_yield Test("top level test", outerSetGenerator);
-	//co_yield Test("inner level test", innerSetGenerator);
+	/**
+	 * \brief Verifies that an empty generator can be executed
+	 */
+	Runner runner(basicGenerator, 0);
+}
 
-	co_return;
-};
+// Tests that creation via literal works
+// auto innerSetGenerator = []() -> TestGenerator
+//{
+//	int count = 0;
+//	co_yield Test(
+//		"test",
+//		[]
+//		{
+//		});
 
-// Registration
-suite suite("test suite", suiteGenerator);
+//	// co_yield "yes"_test = [&count]()
+//	//{
+//	//	++count;
+//	// };
+
+//	// co_yield "array"_test = [&count](const auto& parameter)
+//	//{
+//	//	++count;
+//	// } | std::tuple{3u, 4.0f};
+
+//	// co_yield "array"_test = [&count]<typename T>(const T& parameter)
+//	//{
+//	//	++count;
+//	// } | std::array{3, 4};
+//};
+
+// co_yield Test("top level test", outerSetGenerator);
+// co_yield Test("inner level test", innerSetGenerator);

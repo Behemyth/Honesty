580604d6378d8b92207517a08e9ad8db13834a25
Asher Norland <asher.norland@gmail.com>
2024-06-24 17:03:12 -0400

    Suite Name Tests

--- tests/unit/test/test_suite.cpp
+++ tests/unit/test/test_suite.cpp
@@ -2,14 +2,27 @@ import std;
 import synodic.honesty.test;
 
 using namespace synodic::honesty::test;
+using namespace synodic::honesty::test::literals;
 
 namespace
 {
-	auto suiteGenerator = []() -> Generator
-	{
-		co_return;
-	};
+	Suite SUITE(
+		"suite",
+		[]() -> Generator
+		{
+			co_yield "name"_test = [&](const Requirements& requirements)
+			{
+				constexpr std::string_view name = "test";
+				Suite testSuite(
+					name,
+					[]() -> Generator
+					{
+						co_return;
+					});
 
-	Suite suite("suite", suiteGenerator);
-	bool result = RegisterSuite(suite);
+				requirements.ExpectEquals(testSuite.Name(), name);
+			};
+		});
+
+	SuiteRegistrar _(SUITE);
 }

161519c389dbdba83f97bcb5c64a161e06085390
Asher Norland <asher.norland@gmail.com>
2024-10-26 19:09:35 -0400

    Update test_command.cpp

--- tests/unit/test/test_command.cpp
+++ tests/unit/test/test_command.cpp
@@ -21,14 +21,14 @@ namespace
 			co_yield "list_json"_test = [&](const Requirements& requirements)
 			{
 				const auto path = temporaryPath.generic_string();
-				std::array<std::string_view, 5> arguments {"fakeEXE", "list", "--json", "--file", path};
+				std::array<std::string_view, 5> arguments{"fakeEXE", "list", "--json", "--file", path};
 
 				std::stringstream stringStream;
 				synodic::honesty::log::OStream<std::mutex> stream(stringStream);
 
 				Instance::Configuration configuration("instance_test", &stream);
 				Instance command(configuration, arguments);
-				std::optional<command::ListData> data = command.CommandData<command::List>();
+				const std::optional<command::ListData> data = command.CommandData<command::List>();
 
 				requirements.Assert(data.has_value());
 				requirements.Expect(data->outputType == command::ListOutputType::JSON);

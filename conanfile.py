ea7472cbdf2b3ab0f15d140a5ebb6dcac7eb2308
Asher Norland <asher.norland@gmail.com>
2025-04-15 02:18:33 -0400

    Init Conan

---
+++ conanfile.py
@@ -0,0 +1,22 @@
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+
+class MyProject(ConanFile):
+    name = "myproject" 
+    version = "1.0"
+    settings = "os", "compiler", "build_type", "arch"
+    requires = ['opentelemetry-cpp/[>=1.18.0]']
+    generators = "CMakeDeps"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()

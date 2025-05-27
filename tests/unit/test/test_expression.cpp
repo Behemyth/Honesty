c82fcdb92cfe1e5a884f1a4ad7c360da7896e116
Asher Norland <asher.norland@gmail.com>
2025-05-27 07:58:07 -0700

    Reorganize

--- tests/unit/test/test_expression.cpp
+++ tests/unit/test/test_expression.cpp
@@ -11,33 +11,43 @@ namespace
 		"expectation",
 		[](Fixture& fixture) -> Generator
 		{
-			const std::stringstream& stream = fixture.AttachListener(honesty::log::LevelType::TRACE);
-
 			co_yield "error_description"_test = [&]() -> Generator
 			{
+				const std::stringstream& stream = fixture.AttachListener(honesty::log::LevelType::TRACE);
+
 				// Tests callable for error message        
 				const auto description = []() -> std::string
 				{
-					return "This outputs only on error";
+					return "ERROR";
 				};
 
-				co_yield FAIL / "assert"_test = [&](const Requirements& requirements)
+				co_yield FAIL / "assert_fails"_test = [&](const Requirements& requirements)
 				{
-					requirements.Assert(false, "This outputs only on error");
-
-					requirements.Assert(true, "This outputs only on error");
-
+					requirements.Assert(false, "ERROR");
 					requirements.Assert(false, description);
 				};
 
-				co_yield FAIL / "expect"_test = [&](const Requirements& requirements)
+				co_yield "assert_succeeds"_test = [&](const Requirements& requirements)
 				{
-					requirements.Expect(false, "This outputs only on error");
+					requirements.Assert(true, "ERROR");
+					requirements.Assert(true, description);
+				};
 
-					requirements.Expect(true, "This outputs only on error");
-
+				co_yield FAIL / "expect_fails"_test = [&](const Requirements& requirements)
+				{
+					requirements.Expect(false, "ERROR");
 					requirements.Expect(false, description);
 				};
+
+				co_yield "expect_succeeds"_test = [&](const Requirements& requirements)
+				{
+					requirements.Expect(true, "ERROR");
+					requirements.Expect(true, description);
+				};
+
+				requirements.Expect(
+					stream.view().empty(),
+					"Nothing should be present in the stream.");
 			};
 		});
 

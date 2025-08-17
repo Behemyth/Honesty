252df3de3760675d850b0fc370558930070778bf
Asher Norland <asher.norland@gmail.com>
2025-08-17 05:47:54 -0400

    Package Components

--- conanfile.py
+++ conanfile.py
@@ -30,7 +30,14 @@ class AutoPackage(ConanFile):
 		cmake.install()
 
 	def package_info(self):
-		self.cpp_info.libs = ["honesty"]
+		self.cpp_info.set_property("cmake_file_name", "honesty")
+
+		self.cpp_info.components["honesty"].libs = ["honesty"]
+		self.cpp_info.components["honesty"].set_property("cmake_target_name", "synodic::honesty")
+
+		self.cpp_info.components["main"].libs = ["honesty_main"]
+		self.cpp_info.components["main"].set_property("cmake_target_name", "synodic::honesty::main")
+		self.cpp_info.components["main"].requires = ["honesty"]
 
 	def export_sources(self):
 		copy(self, "CMakeLists.txt", src=self.recipe_folder, dst=self.export_sources_folder)
